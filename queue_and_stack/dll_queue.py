04c96d6817b74a9cdf90e011c07a6c4e633a1f35
Ronnie <ronniedipple@gmail.com>
2020-02-24 17:33:04 -0600

    Guided project complete

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -2,6 +2,9 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+# Queues, like the name suggests, follow the First-in-First-Out (FIFO) principle.
+# As if waiting in a queue for the movie tickets,
+# the first one to stand in line is the first one to buy a ticket and enjoy the movie.
 
 class Queue:
     def __init__(self):
@@ -9,11 +12,14 @@ class Queue:
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
 
+    # enqueue() : Adds element to the back of Queue.
     def enqueue(self, value):
         pass
 
+    # dequeue() : Removes and returns the first element from the queue.
     def dequeue(self):
         pass
 
+    # len returns the number of items in the queue.
     def len(self):
         pass
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,17 +2,26 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+# Stacks, like the name suggests, follow the Last-in-First-Out (LIFO) principle.
+# As if stacking coins one on top of the other,
+# the last coin we put on the top is the one that is the first to be removed
+# from the stack later.
+
+
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
 
+    # push : adds to front of a list
     def push(self, value):
         pass
 
+    # pop(): removes and returns last value from the list or the given index value.
     def pop(self):
         pass
 
+    # len returns the number of items in the queue.
     def len(self):
         pass
