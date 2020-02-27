b833818c6d8438ec44fb80c83048dd9d253acd5c
Ronnie <ronniedipple@gmail.com>
2020-02-27 17:39:18 -0600

    binary search tree day 2 complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -81,34 +81,121 @@ class BinarySearchTree:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        if node:
-            self.in_order_print(self.right)
-            print(self.value)
-            self.in_order_print(self.left)
+        final = []
+
+        def iop_helper(node):
+            nonlocal final
+            if node:
+                iop_helper(node.left)
+
+                final.append(node.value)
+
+                iop_helper(node.right)
+
+        iop_helper(node)
+        for result in final:
+            print(result)
+        return final
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        if node:
-            print(node)
-            print(self.left)
-            print(self.right)
+        stack = Queue()
+        # root into stack
+        stack.enqueue(node)
+        #print(f"init stack len ={stack.len}")
+        while stack.len != 0:
+            temp: BinarySearchTree = stack.dequeue()
+            if temp:
+                if temp.right:
+                    stack.enqueue(temp.right)
+                if temp.left:
+                    stack.enqueue(temp.left)
+                print(temp.value)
+            else:
+                break
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = Stack()
+        # root into stack
+        stack.push(node)
+        #print(f"init stack len ={stack.len}")
+        while stack.len != 0:
+            temp: BinarySearchTree = stack.pop()
+            if temp:
+                if temp.right:
+                    stack.push(temp.right)
+                if temp.left:
+                    stack.push(temp.left)
+                print(temp.value)
+            else:
+                break
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
     def pre_order_dft(self, node):
-        pass
+        final = []
+
+        def preorder_helper(node):
+            nonlocal final
+            if node:
+                final.append(node.value)
+
+                preorder_helper(node.left)
+
+                preorder_helper(node.right)
+
+        preorder_helper(node)
+        for result in final[0:]:
+            print(result)
+        return final
 
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
-        pass
+        final = []
+
+        def post_order_helper(node):
+            nonlocal final
+            if node:
+                post_order_helper(node.left)
+
+                post_order_helper(node.right)
+
+                final.append(node.value)
+
+        post_order_helper(node)
+        for result in final[0:]:
+            print(result)
+        return final
+
+
+class test():
+    def try1(self):
+        self.bst = BinarySearchTree(1)
+        self.bst.insert(8)
+        self.bst.insert(5)
+        self.bst.insert(7)
+        self.bst.insert(6)
+        self.bst.insert(3)
+        self.bst.insert(4)
+        self.bst.insert(2)
+        # print(f"in order print return ="
+        #       f"{self.bst.in_order_print(self.bst)}")
+        # print(f"pre order print return ="
+        #       f"{self.bst.pre_order_dft(self.bst)}")
+        # print(f"post order print return ="
+        #       f"{self.bst.post_order_dft(self.bst)}")
+        print(f"^dft print^ ="
+              f"{self.bst.dft_print(self.bst)}")
+
+
+test = test()
+
+test.try1()
 
 # bst = BinarySearchTree()
 # bst.insert(4)
@@ -116,3 +203,25 @@ class BinarySearchTree:
 # bst.insert(8)
 # bst.insert(5)
 # bst.insert(10)
+
+
+
+#DFT Steps
+# initialize a stack
+# push root to stack
+# while stack not empty
+# pop top item of stack into temp
+# do the thing
+# if temp has right put in stack
+# if temp has left put into stack
+#
+
+# BFT going wide aka doing each level at a time
+# initialize a queue
+# push root to queue
+# while queue not empty
+# pop top item of queue into temp
+# do the thing
+# if temp has right put in queue
+# if temp has left put into queue
+#
